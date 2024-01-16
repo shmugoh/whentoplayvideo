ac88530da6448e8a71c5296b596cdef13f684504
Juan P. <juan@shmugo.co>
2024-01-15 19:06:23 -0500

    undo video mobile changes

--- components/layout/video.tsx
+++ components/layout/video.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import YTPlayer from "yt-player";
 
 type EmbedProps = {
@@ -9,60 +9,33 @@ type EmbedProps = {
 };
 
 export function YouTubeVideoEmbed(props: EmbedProps) {
-  const playerRef = useRef<HTMLDivElement>(null);
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  const isMobile = window.innerWidth <= 768; // Adjust the breakpoint as needed
-  const videoSizePercentage = 40;
+  const playerRef = useRef("");
 
   useEffect(() => {
-    const player = new YTPlayer(playerRef.current!, {
-      controls: true,
-      width: isMobile ? "100%" : (windowSize.width * videoSizePercentage) / 100, // Set width to 100% for mobile, 60% for other screens
-      height: isMobile
-        ? windowSize.width * 0.5625
-        : (windowSize.height * videoSizePercentage) / 100, // Set height based on aspect ratio for mobile, 30% for other screens
+    // Create a new player instance when the component mounts
+    const player = new YTPlayer(playerRef.current, {
+      controls: true, // Add any additional configurations you need
     });
 
-    const handleResize = () => {
-      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
-    };
-
-    window.addEventListener("resize", handleResize);
-
+    // Load Video
     player.load(props.videoId);
-
     player.on("unstarted", () => {
-      const durationVal = player.getDuration();
+      // Obtain Duration
+      var durationVal = player.getDuration();
       props.setDurationHook(durationVal);
     });
 
     player.on("timeupdate", () => {
-      const currentTime = Math.floor(player.getCurrentTime());
+      // Update Current Time Update
+      var currentTime = Math.floor(player.getCurrentTime());
       props.setLengthHook(currentTime);
     });
 
+    // Destroy when new videoId is parsed
     return () => {
       player.destroy();
-      window.removeEventListener("resize", handleResize);
     };
-  }, [props.videoId, windowSize]);
-
-  // sets resolution dynamically
-  useEffect(() => {
-    if (playerRef.current) {
-      const playerElement = playerRef.current;
-      const isMobile = window.innerWidth <= 768; // Adjust the breakpoint as needed
-      playerElement.style.width = isMobile
-        ? "100%"
-        : `${(windowSize.width * videoSizePercentage) / 100}px`; // Set width to 100% for mobile, 60% for other screens
-      playerElement.style.height = isMobile
-        ? `${windowSize.width * 0.5625}px`
-        : `${(windowSize.height * videoSizePercentage) / 100}px`; // Set height based on aspect ratio for mobile, 30% for other screens
-    }
-  }, [windowSize]);
+  }, [props.videoId]);
 
   return <div className={props.className} ref={playerRef}></div>;
 }

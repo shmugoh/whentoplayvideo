728de21516152b1c766b06c838f5ed216c0eed55
Juan P <juan@shmugo.co>
2024-01-19 04:35:13 -0500

    refactor time output styling
    it's 5am but at least i can sleep in peace knowing that width wont have a stroke anymore

--- utils/calculateTime.ts
+++ utils/calculateTime.ts
@@ -1,10 +1,18 @@
+type TimeCalculationOutput = {
+  hour: string;
+  minute: string;
+  second: string;
+  meridiem: string;
+};
+
 export function calculateTime(
   videoTime: number,
   timeHour: number,
   timeMinute: number,
   timeSecond: number,
   meridiem: string
-): string {
+): TimeCalculationOutput {
+  // TODO: add type of this function
   // for pre-set values in carosuel so it can do the calculations
   if (timeHour == 0) {
     timeHour = 12;
@@ -34,5 +42,10 @@ export function calculateTime(
   const formattedMinutes = finalMinutes.toString().padStart(2, "0");
   const formattedSecs = finalSecs.toString().padStart(2, "0");
 
-  return `${formattedHours}:${formattedMinutes}:${formattedSecs} ${finalMeridiem}`;
+  return {
+    hour: formattedHours,
+    minute: formattedMinutes,
+    second: formattedSecs,
+    meridiem: meridiem,
+  };
 }

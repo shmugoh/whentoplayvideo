528b64eb3996519f50a255b2fd6650fa5466dd01
Juan P <juan@shmugo.co>
2024-01-22 15:12:27 -0500

    refactor footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,16 @@
 
 import React, { useState, useEffect } from "react";
 
-import { useTheme } from "next-themes";
-
 import { InputForm } from "@/components/layout/form";
 import { YouTubeVideoEmbed } from "@/components/layout/youtube-video";
 import { TimeSlot } from "@/components/layout/time";
 
+import { Foot } from "@/components/footer";
+
 import { calculateTime } from "@/utils/calculateTime";
-
-import { Moon, Sun, Github, Coffee } from "lucide-react";
-
-import { Separator } from "@/components/ui/separator";
-
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   /* Hooks */
-  // UI Hooks
-  const { theme, setTheme } = useTheme();
-
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
   // YouTube Hooks
   const [videoId, setVideoId] = React.useState("");
   const [duration, setDuration] = React.useState(0);
@@ -95,48 +82,7 @@ export default function Home() {
           </>
         )}
       </div>
-
-      <Separator className="bg-primary" />
-      <footer className="bg-grey-200 relative mt-auto py-6 md:px-8 md:py-0">
-        <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-          <div className="flex flex-wrap justify-center">
-            <ul className="flex items-center space-x-4">
-              <p className="leading-7 [&:not(:first-child)]:mt-6">
-                Made with ❤️ in Colombia by{" "}
-                <a
-                  href="https://twitter.com/shmugo_"
-                  className="font-medium text-primary underline underline-offset-4"
-                >
-                  shmugo
-                </a>
-              </p>
-            </ul>
-          </div>
-
-          <div className="flex items-center space-x-4">
-            <a
-              href="https://ko-fi.com/shmugo"
-              className="flex space-x-2 relative"
-            >
-              <p className="flex font-medium text-primary underline underline-offset-4">
-                Buy me a Coffee
-              </p>
-              <Coffee />
-            </a>
-            <Separator className="h-8" orientation="vertical" />
-            <a href="https://github.com/shmugoh/whentoplayvideo">
-              <Github />
-            </a>
-            <Separator className="h-8" orientation="vertical" />
-
-            {theme === "light" ? (
-              <Moon onClick={toggleTheme} />
-            ) : (
-              <Sun onClick={toggleTheme} />
-            )}
-          </div>
-        </div>
-      </footer>
+      <Foot />
     </main>
   );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useTheme } from "next-themes";
+import { Separator } from "@/components/ui/separator";
+import { Moon, Sun, Github, Coffee } from "lucide-react";
+
+export function Foot() {
+  // UI Hooks
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <div>
+      <Separator className="bg-primary" />
+      <footer className="bg-grey-200 relative mt-auto py-6 md:px-8 md:py-0">
+        <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+          <div className="flex flex-wrap justify-center">
+            <ul className="flex items-center space-x-4">
+              <p className="leading-7 [&:not(:first-child)]:mt-6">
+                Made with ❤️ in Colombia by{" "}
+                <a
+                  href="https://twitter.com/shmugo_"
+                  className="font-medium text-primary underline underline-offset-4"
+                >
+                  shmugo
+                </a>
+              </p>
+            </ul>
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <a
+              href="https://ko-fi.com/shmugo"
+              className="flex space-x-2 relative"
+            >
+              <p className="flex font-medium text-primary underline underline-offset-4">
+                Buy me a Coffee
+              </p>
+              <Coffee />
+            </a>
+            <Separator className="h-8" orientation="vertical" />
+            <a href="https://github.com/shmugoh/whentoplayvideo">
+              <Github />
+            </a>
+            <Separator className="h-8" orientation="vertical" />
+
+            {theme === "light" ? (
+              <Moon onClick={toggleTheme} />
+            ) : (
+              <Sun onClick={toggleTheme} />
+            )}
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}

aa7c19805dc1ddd9e0c8e98534f01bc74a264bec
Juan P <juan@shmugo.co>
2024-01-22 23:15:12 -0500

    refactor footer socials & separator color

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,17 +5,28 @@ import { useTheme } from "next-themes";
 import { Separator } from "@/components/ui/separator";
 import { Moon, Sun, Github, Coffee } from "lucide-react";
 
+const links = [
+  {
+    href: "https://ko-fi.com/shmugo",
+    text: "Buy me a Coffee",
+    icon: <Coffee />,
+  },
+  {
+    href: "https://github.com/shmugoh/whentoplayvideo",
+    icon: <Github />,
+  },
+];
+
 export function Foot() {
   // UI Hooks
   const { theme, setTheme } = useTheme();
-
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
     <div>
-      <Separator className="bg-border" />
+      <Separator />
       <footer className="bg-muted relative mt-auto py-6 md:px-8 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
           <div className="flex flex-wrap justify-center">
@@ -33,20 +44,28 @@ export function Foot() {
           </div>
 
           <div className="flex items-center space-x-4">
-            <a
-              href="https://ko-fi.com/shmugo"
-              className="flex space-x-2 relative"
-            >
-              <p className="flex font-medium text-muted-foreground underline underline-offset-4">
-                Buy me a Coffee
-              </p>
-              <Coffee className="hover:-rotate-6 hover:scale-125" />
-            </a>
-            <Separator className="h-8" orientation="vertical" />
-            <a href="https://github.com/shmugoh/whentoplayvideo">
-              <Github className="hover:rotate-6 hover:scale-125" />
-            </a>
-            <Separator className="h-8" orientation="vertical" />
+            {links.map(({ href, text, icon }, index) => (
+              <React.Fragment key={index}>
+                <a href={href} className="flex space-x-2 relative">
+                  {text && (
+                    <p className="flex font-medium text-muted-foreground underline underline-offset-4">
+                      {text}
+                    </p>
+                  )}
+                  {React.cloneElement(icon, {
+                    className: "hover:-rotate-6 hover:scale-125",
+                  })}
+                </a>
+                {index < links.length - 1 && (
+                  <Separator
+                    className="h-8 bg-popover"
+                    orientation="vertical"
+                  />
+                )}
+              </React.Fragment>
+            ))}
+
+            <Separator className="h-8 bg-popover" orientation="vertical" />
 
             {theme === "light" ? (
               <Moon
